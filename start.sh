89488ebe8de4a4d39aa7679d4b505d8d582c8e5c
Kyle Umstatter <kumstatter@genvidtech.com>
2020-03-29 12:55:03 -0700

    update nomad version

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/usr/local/bin/dumb-init /bin/sh
-# Script created following Hashicorp's model for Consul: 
+# Script created following Hashicorp's model for Consul:
 # https://github.com/hashicorp/docker-consul/blob/master/0.X/docker-entrypoint.sh
 # Comments in this file originate from the project above, simply replacing 'Consul' with 'Nomad'.
 set -e
@@ -19,7 +19,7 @@ NOMAD_CONFIG_DIR=${NOMAD_CONFIG_DIR:-"/etc/nomad"}
 # You can also set the NOMAD_LOCAL_CONFIG environemnt variable to pass some
 # Nomad configuration JSON without having to bind any volumes.
 if [ -n "$NOMAD_LOCAL_CONFIG" ]; then
-	echo "$NOMAD_LOCAL_CONFIG" > "$NOMAD_CONFIG_DIR/local.json"
+    echo "$NOMAD_LOCAL_CONFIG" >"$NOMAD_CONFIG_DIR/local.json"
 fi
 
 # If the user is trying to run Nomad directly with some arguments, then
